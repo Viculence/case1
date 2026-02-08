7664f83c9279609782b446e152fed4247285546f
Viktoriya <viculence@mail.ru>
2026-02-08 22:42:44 +0700

    Added the base code for the functions.

---
+++ main.py
@@ -0,0 +1,119 @@
+# Case-study #1
+# Developers: Burykhina E., Yasminskaya V.
+
+import turtle
+
+def square(x, y, a, color):
+    '''
+    Function to draw a square.
+    :param x: x-coordinate of the top-left corner
+    :param y: y-coordinate of the top-left corner
+    :param a: side length of the square
+    :param color: color of the square
+    :return: None
+    '''
+    # Вика
+    pass
+
+def circle(x, y, radius, color):
+    '''
+    Function to draw a circle.
+    :param x: x-coordinate of the top-left center
+    :param y: y-coordinate of the top-left center
+    :param radius: radius of the circle
+    :param color: color of the circle
+    :return: None
+    '''
+    # Мила
+    pass
+
+def rectangle(x, y, width, height, color):
+    '''
+    Function to draw a rectangle.
+    :param x: x-coordinate of the top-left corner
+    :param y: y-coordinate of the top-left corner
+    :param width: width of the rectangle
+    :param height: height of the rectangle
+    :param color: color of the rectangle
+    :return: None
+    '''
+    # Мила
+    pass
+
+def parallelogram(x, y, base, heigh, angle, color):
+    '''
+    Function to draw a parallelogram.
+    :param x: x-coordinate of the top-left corner
+    :param y: y-coordinate of the top-left corner
+    :param base: length of the base of the parallelogram
+    :param heigh: height of the parallelogram
+    :param angle: angle between the base and the sides
+    :param color: color of the parallelogram
+    :return: None
+    '''
+    # Мила
+    pass
+
+def rhombus(x, y, diagonal1, diagonal2, color):
+    '''
+    Function to draw a rhombus.
+    :param x: x-coordinate of the top-left corner
+    :param y: y-coordinate of the top-left corner
+    :param diagonal1: length of the first diagonal
+    :param diagonal2: length of the second diagonal
+    :param color: color of the rhombus
+    :return: None
+    '''
+    # Мила
+    pass
+
+def equilateral_triangle(x, y, a, color):
+    '''
+    Function to draw a equilateral triangle.
+    :param x: x-coordinate of the top point of the triangle
+    :param y: y-coordinate of the top point of the triangle
+    :param a: side length of the triangle
+    :param color: color of the equilateral triangle
+    :return: None
+    '''
+    # Вика
+    pass
+
+def right_triangle(x, y, base, height, color):
+    '''
+    Function to draw a right triangle.
+    :param x: x-coordinate of the vertex of the triangle
+    :param y: y-coordinate of the vertex of the triangle
+    :param base: length of the base
+    :param height: height of the triangle
+    :param color: color of the right triangle
+    :return: None
+    '''
+    # Вика
+    pass
+
+def fish():
+    # Мила
+    pass
+
+def car():
+    # Мила
+    pass
+
+def butterfly():
+    # Вика
+    pass
+
+def candle():
+    # Вика
+    pass
+
+def main():
+    fish()
+    car()
+    butterfly()
+    candle()
+
+    turtle.done()
+
+main()
